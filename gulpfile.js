14359669556be4c50b3f271d514990dfd741a64e
Dominick Peluso <peluso.dominick@gmail.com>
2016-05-13 11:11:20 -0400

    Add gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 
+var paths = {
+    emails: ['./src/emails/**/*.jade'],
+    view: './src/view/**/*.jade'
+};
+
 gulp.task('templates', function() {
   var YOUR_LOCALS = {};
 
-  gulp.src('./src/emails/**/*.jade')
+  gulp.src(paths.emails)
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
     .pipe(gulp.dest('./dist/'))
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(paths.emails, ['templates']);
+    gulp.watch(paths.view, ['templates']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['templates', 'watch']);
